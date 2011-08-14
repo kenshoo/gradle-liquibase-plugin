9df70ef3c7afdeda3214dc6eff3110a4f7720e34
narkisr <narkisr@gmail.com>
2011-08-14 20:00:07 +0300

    asserting custom refactoring in update

--- src/test/resources/changelogs/play-changelog.groovy
+++ src/test/resources/changelogs/play-changelog.groovy
@@ -1,22 +1,18 @@
 databaseChangeLog() {
 
-  importCustom(rename:'liquibase.custom.ExampleCustomChange') 
+  importCustom(addData:'liquibase.custom.ExampleCustomChange') 
 
   changeSet(id: 'create-table', author: 'tlberglund') {
     createTable(tableName: 'play', remarks: 'do we care?') {
       column(name:'id',type:'int')
       column(name:'name',type:'varchar(255)')
     }
-   insert(tableName:"play"){
-    column(name:"id", valueNumeric:1)
-    column(name:"name", value:"haim")
-   }
   }
 
   changeSet(id: 'bla', author: 'ronen') {
-   rename(tableName:'play'){
-       columnName("name") 
-       newValue("'moshe'")
+   addData(tableName:'play'){
+       columnName("id") 
+       newValue(1)
    }
   }
 }
