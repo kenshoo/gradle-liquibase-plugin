3506cb2c9d9b3f386a82a077352a7134af5b7c2d
narkisr <narkisr@gmail.com>
2011-08-14 17:37:08 +0300

    custom changes dsl work

--- src/test/resources/changelogs/play-changelog.groovy
+++ src/test/resources/changelogs/play-changelog.groovy
@@ -1,9 +1,23 @@
 databaseChangeLog() {
 
-  changeSet(id: 'create-table', author: 'tlberglund') {
-    createTable( tableName: 'play', remarks: 'do we care?') {
-      column(name:'id',type:'int')
+  importCustom(rename:'liquibase.custom.ExampleCustomChange') 
 
+  changeSet(id: 'create-table', author: 'tlberglund') {
+    createTable(tableName: 'play', remarks: 'do we care?') {
+      column(name:'id',type:'int')
+      column(name:'name',type:'varchar(255)')
     }
+   insert(tableName:"play"){
+    column(name:"id", valueNumeric:1)
+    column(name:"name", value:"haim")
+   }
+  }
+
+  changeSet(id: 'bla', author: 'ronen') {
+   rename {
+       tableName("play")
+       columnName("name") 
+       newValue("'moshe'")
+   }
   }
 }
