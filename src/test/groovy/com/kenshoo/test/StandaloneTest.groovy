671cb05d6e3bdf93e61872777ae5561b912a6384
narkisr <narkisr@gmail.com>
2011-08-23 20:50:01 +0300

    update seems to be working

--- src/test/groovy/com/kenshoo/test/StandaloneTest.groovy
+++ src/test/groovy/com/kenshoo/test/StandaloneTest.groovy
@@ -18,13 +18,19 @@ import org.gradle.api.Project
 class StandaloneTest {
 
  def dyanmicProperties = DyanmicProperties.instance
+ def project
+ def standalone
+
+ @Before
+ public void setup(){
+    standalone = new Standalone()
+    project = standalone.instance()
+    def plugin = new LiquibasePlugin()
+    plugin.apply(project)
+ }
 
  @Test
  public void apply(){
-    def standalone = new Standalone()
-    def plugin = new LiquibasePlugin()
-    def project = standalone.instance()
-    plugin.apply(project)
     assertThat project.convention.plugins.liqui, is(notNullValue())
     assertThat dyanmicProperties.properties.keySet(),hasItems('update','reportStatus')
     assertThat standalone.tasks.update.description, containsString('Applying all changesets with given contexts to the db')
@@ -32,14 +38,18 @@ class StandaloneTest {
   
  @Test
  public void genConf(){
-     def standalone = new Standalone()
-     def plugin = new LiquibasePlugin()
-     def project = standalone.instance()
-     plugin.apply(project)
      project.dbName = 'play'
      assertThat dyanmicProperties.properties.dbName,is(notNullValue())
      assertThat project.hasProperty('dbName'),is(true)
-     standalone.tasks.genConf.doLast.call()
+     standalone.tasks.genConf.doLast()
      assertThat new File('liquid.conf').text, containsString("name: 'play'")
+ }
+
+ @Test
+ public void update(){
+     def liqui = project.convention.plugins.liqui
+     liqui.configurationScript = 'src/test/resources/liquid.conf'
+     project.contexts = ''
+     standalone.tasks.update.doLast()
  }
 }
