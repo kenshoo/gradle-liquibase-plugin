0e4745adfd25e4e223d4bcf2bcf7d30af73cb56b
narkisr <narkisr@gmail.com>
2011-08-24 13:46:11 +0300

    args parsing works

--- src/main/groovy/com/kenshoo/liquibase/Main.groovy
+++ src/main/groovy/com/kenshoo/liquibase/Main.groovy
@@ -9,9 +9,14 @@ import liquibase.parser.ext.GroovyLiquibaseChangeLogParser
 import liquibase.resource.FileSystemResourceAccessor
 import org.gradle.api.Project
 import com.kenshoo.gradle.Standalone
+import com.kenshoo.gradle.ArgsParser
 
 class Main {
     public static void main(String[] args) {
-       new LiquibasePlugin().apply(new Standalone().instance())
+    	 def task = args.first()
+       def project = new Standalone().instance()
+    	 new ArgsParser().apply(project,args) 
+       new LiquibasePlugin().apply(project)
+       project."$task".doLast()
     }
 }
