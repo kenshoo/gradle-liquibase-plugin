57f0da8f01addc4d1e0ee39660bcc16c7adb4d09
narkisr <narkisr@gmail.com>
2011-08-24 15:23:45 +0300

    adding tasks task

--- src/main/groovy/com/kenshoo/gradle/Standalone.groovy
+++ src/main/groovy/com/kenshoo/gradle/Standalone.groovy
@@ -3,23 +3,34 @@ package com.kenshoo.gradle
 import org.gradle.api.Project
 import org.gradle.api.plugins.Convention 
 import org.gradle.api.Task
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 
 
 class Standalone {
   
+  def Logger logger = LoggerFactory.getLogger(this.class);
+
   def plugins = [:]
-  def tasks = [:]
+  def tasks = [tasks:[:]]
+  def printTasks = {
+    tasks.each { name,props->
+      println("${name} - ${props.description}")    
+    }
+  }
   def convention = [getPlugins:{plugins}] as  Convention
   def dyanmicProperties = DyanmicProperties.instance.properties
     
   def instance() {
+    tasks['tasks'].doLast = printTasks
+    tasks.tasks.description = "tasks - Prints the list of available tasks and input parameters.\n"
     def project = [ 
 	getConvention: {convention},
 	task: { props,name -> 
-	  tasks[name]=props
+  	    tasks[name]=props
 	    def task = [leftShift: {c->
 		tasks[name].doLast=c
-		  delegate as Task 
+		delegate as Task 
 	    }] 
 	  task as Task
 	},
